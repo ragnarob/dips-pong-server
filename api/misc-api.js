51ece544a8409ae05c18f0e30ab092ec6e4af756
ragnarob <ragnarob@protonmail.com>
2019-10-03 00:04:17 +0200

    Add rivalries

--- api/misc-api.js
+++ api/misc-api.js
@@ -11,12 +11,12 @@ module.exports = class MiscApi {
   }
 
   setupRoutes () {
-    this.app.get('/api/hotstreaks', async (req, res) => {
+    this.app.get('/api/otherStats', async (req, res) => {
       if (!req.query || !req.query.officeId) {
         res.json({error: 'Missing query parameter officeId'})
       }
-      let streaks = await this.getHotStreaks(req.query.officeId)
-      res.json(streaks)
+      let otherStats = await this.getOtherStats(req.query.officeId)
+      res.json(otherStats)
     })
 
     this.app.get('/api/ratingstats', async (req, res) => {
@@ -43,28 +43,93 @@ module.exports = class MiscApi {
     })
   }
 
-  async getHotStreaks (officeId) {
+  async getOtherStats (officeId) {
     let allPlayers = await this.playerApi.getAllPlayers(officeId)
     let streakList = []
+    let allRivalries = []
 
     for (const player of allPlayers) {
       let playerStats = await this.playerApi.getPlayerStats(officeId, player.name)
-
+      let rivalries = {}
+      let streakStop = false
       let streak = 0
       for (const game of playerStats.matches) {
-        if (game.winningPlayer === player.name) {
+        let isVictory = game.winningPlayer === player.name
+        let opponent = isVictory ? game.losingPlayer : game.winningPlayer
+        if (!(opponent in rivalries)) { rivalries[opponent] = {win: 0, lose: 0} }
+        rivalries[opponent][isVictory ? 'win' : 'lose'] += 1
+
+        if (!streakStop && game.winningPlayer === player.name) {
           streak++
         }
         else {
-          break
+          streakStop = true
         }
+      }
+
+      let topRivalries = this.getTopRivalries(rivalries, player.name)
+      if (topRivalries) {
+        allRivalries.push(...topRivalries)
       }
 
       streakList.push({name: player.name, streak: streak})
     }
 
-    let topThree = streakList.sort((p1, p2) => p1.streak>p2.streak ? -1 : 1)
-    return topThree.slice(0, 3).filter(t => t.streak > 1)
+    let topThreeRivalries = this.findTopTotalRivalries(allRivalries)
+    let topThreeStreaks = streakList.sort((p1, p2) => p1.streak>p2.streak ? -1 : 1)
+
+    return {
+      streaks: topThreeStreaks.slice(0, 3).filter(t => t.streak > 1),
+      rivalries: topThreeRivalries
+    }
+  }
+
+  getTopRivalries (opponentsMatchList, playerName) {
+    let scores = []
+    for (let player in opponentsMatchList) {
+      let numbers = opponentsMatchList[player]
+      if (numbers.win + numbers.lose > 5) {
+        let score = this.calculateRivalryScore(numbers.win, numbers.lose)
+        scores.push([player, score])
+      }
+    }
+    if (scores.length === 0) {
+      return
+    }
+
+    scores.sort((s1, s2) => s1[1] > s2[1] ? -1 : 1)
+    let topScores = scores.splice(0,3)
+    let topRivalries = topScores.map(
+      topScore => ({
+        player: playerName,
+        score: topScore[1],
+        opponent: topScore[0],
+        results: opponentsMatchList[topScore[0]]
+      })
+    )
+    return topRivalries
+  }
+
+  calculateRivalryScore (wins, losses) {
+    let total = wins + losses
+    let diff = Math.abs(wins - losses)
+    return total - ((diff + 1) * total) / 5
+  }
+
+  findTopTotalRivalries (rivalryList) {
+    let finalRivalries = []
+    rivalryList.sort((r1, r2) => r1.score > r2.score ? -1 : 1)
+
+    for (let i=0; i<rivalryList.length; i+=2) {
+      let playerIsLeading = rivalryList[i].results.win >= rivalryList[i].results.lose
+      finalRivalries.push({
+        'p1': playerIsLeading ? rivalryList[i].player : rivalryList[i].opponent,
+        'p2': playerIsLeading ? rivalryList[i].opponent : rivalryList[i].player,
+        'games': [rivalryList[i].results.win, rivalryList[i].results.lose]
+      })
+    }
+
+    return finalRivalries
   }
 
   async getRatingStats (officeId) {
